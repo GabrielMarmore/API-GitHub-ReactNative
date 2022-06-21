9a9599e6de04d2aeda9f69b1ec0dd8fa7e481208
Gabriel Marmore <gabrielpiraja@hotmail.com>
2022-06-20 21:40:58 -0300

    initial_commit

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios'
+import React, { Component, useState, useEffect } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator
+} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function App () {
+  const baseURL = 'https://api.github.com/users/GabrielMarmore/repos'
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    loadRepos()
+  }, [])
+
+  async function loadRepos () {
+    if (loading) return;
+
+    setLoading(true)
+
+    const response = await axios.get(baseURL)
+    setData([...data, ...response.data])
+  }
+  
+  renderItem = ({ item }) => (
+    <View style={styles.listItem}>
+      <Text>{item.full_name}</Text>
     </View>
   );
+
+  return (
+    <FlatList
+      style={{ marginTop: 30 }}
+      contentContainerStyle={styles.list}
+      data={data}
+      renderItem={this.renderItem}
+      keyExtractor={item => String(item.id)}
+    />
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  list: {
+    paddingHorizontal: 20
   },
-});
+
+  listItem: {
+    backgroundColor: '#EEE',
+    marginTop: 20,
+    padding: 30
+  },
+  loading: {
+    alignSelf: 'center',
+    marginVertical: 20
+  }
+})
